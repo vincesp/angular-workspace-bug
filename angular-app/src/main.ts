ef54c443126829ed6bd8cb300da203a677e46353
Vincent Spallek <vincent.spallek@gmail.com>
2025-01-22 17:08:42 +0000

    Move import of my-font-css from ts to scss file

--- angular-app/src/main.ts
+++ angular-app/src/main.ts
@@ -1,5 +1,4 @@
 import { bootstrapApplication } from '@angular/platform-browser'
-import 'my-font-css'
 import { AppComponent } from './app/app.component'
 import { appConfig } from './app/app.config'
 
